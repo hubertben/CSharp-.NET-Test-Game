f59ada443656ef76d7c89fc68380e1a63d0cd59b
Ben <hubertben365@gmail.com>
2023-04-02 23:09:10 -0400

    #

---
+++ testGame/Circle.cs
@@ -0,0 +1,49 @@
+﻿
+using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace testGame
+{
+    public class Circle
+    { 
+
+        private float x, y;
+        private float radius;
+        private Color color;
+        private Ellipse ellipse;
+   
+        public Circle(float x_, float y_, float radius_, Color color_, Canvas canvas_)
+        {
+            x = x_;
+            y = y_;
+            radius = radius_;
+            color = color_;
+
+
+            ellipse = new Ellipse();
+            ellipse.Width = radius * 2;
+            ellipse.Height = radius * 2;
+            
+            SolidColorBrush brush = new SolidColorBrush(color);
+            ellipse.Fill = brush;
+
+            canvas_.Children.Add(ellipse);
+        }
+
+        public void draw()
+        {
+            Canvas.SetLeft(ellipse, x);
+            Canvas.SetTop(ellipse, y);
+        }
+
+        public void move(float dx, float dy)
+        {
+            x += dx;
+            y += dy;
+        }
+
+    }
+}
---
+++ testGame/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿
+using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Threading;
+
+namespace testGame
+{
+    public partial class MainWindow : Window
+    {
+        public Circle C;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            C = new Circle(100, 100, 100, makeColor(128, 230, 255), MyCanvas);
+            
+            DispatcherTimer gameTimer = new DispatcherTimer();
+            gameTimer.Interval = TimeSpan.FromSeconds(0.01);
+            gameTimer.Tick += tick;
+            gameTimer.Start();
+        }
+
+        private void tick(object sender, EventArgs e)
+        {
+            update();
+            draw();
+        }
+
+        private void update()
+        {
+            C.move(1, 0.5f); 
+        }
+        private void draw()
+        {
+            C.draw();
+        }
+
+        public Color makeColor(int r = 0, int g = 0, int b = 0, int a = 255) {
+            Color C = Color.FromArgb((byte)a, (byte)r, (byte)g, (byte)b);
+            return C;
+        }
+    }
+
+}
